18e81945d23d49890f88fb2a056f351c3bd4e212
unknown <nanaion.xp@gmail.com>
2026-09-16 09:51:31 +0000

    Frontend

--- devtrack-backend/src/app.ts
+++ devtrack-backend/src/app.ts
@@ -15,6 +15,7 @@ app.use(
       "http://127.0.0.1:5501",
       "http://localhost:3000",
       "http://127.0.0.1:3000",
+      "https://devtrack-svg7.onrender.com",
     ],
     credentials: true,
   })
